7cfa2d23ed986c4afdd7450ba247c29c9db7a8e3
changu0923 <152964087+changu0923@users.noreply.github.com>
2024-03-11 21:12:16 +0900

    Test : All Wheel Selected

--- Assets/Scripts/TankWheelCollider.cs
+++ Assets/Scripts/TankWheelCollider.cs
@@ -1,18 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TankWheelCollider : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum Axel
     {
-        
+        Front,
+        Rear
     }
 
-    // Update is called once per frame
-    void Update()
+    [Serializable]
+    public struct Wheel
     {
-        
+        public GameObject wheelModel;
+        public WheelCollider wheelCollider;
+        public Axel axel;
+    }
+
+    public float maxAccelration = 30.0f;
+    public float brakeAccelration = 50.0f;
+
+    public List<Wheel> wheels;
+
+    float moveInput;
+
+    private Rigidbody carRb;
+
+    private void Start()
+    {
+        carRb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        GetInputs();
+    }
+
+    private void LateUpdate()
+    {
+        Move();
+    }
+
+    void GetInputs()
+    {
+        moveInput = Input.GetAxis("Vertical");
+    }
+
+    private void Move()
+    {
+        foreach(var wheel in wheels) 
+        {
+            wheel.wheelCollider.motorTorque = moveInput * 600 * maxAccelration * Time.deltaTime;
+        }
     }
 }

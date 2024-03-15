ad86c4ae7b75c7da49cd2050de603c43ec3fd98d
changu0923 <152964087+changu0923@users.noreply.github.com>
2024-03-15 21:45:29 +0900

    Added : turret Lock

--- Assets/Scripts/TankTurretMovement.cs
+++ Assets/Scripts/TankTurretMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TankTurretMovement : MonoBehaviour
 {
@@ -12,9 +13,12 @@ public class TankTurretMovement : MonoBehaviour
 
     [Header("주포 내림각 제한")]
     [SerializeField] float minGunAngle;
+
     [Header("주포 올림각 제한")]
     [SerializeField] float maxGunAngle;
     private int playerLayer;
+
+    private bool isTurretLock;
 
     private void Start()
     {
@@ -29,24 +33,28 @@ public class TankTurretMovement : MonoBehaviour
 
     public void TurretMove()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
+        if (isTurretLock==false)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, ~playerLayer))
-        {
-            Vector3 targetPos = hit.point;
-            aimTransform.position = targetPos;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ~playerLayer))
+            {
+                Vector3 targetPos = hit.point;
+                aimTransform.position = targetPos;
+            }
+            else
+            {
+                Vector3 targetPos = ray.origin + ray.direction * 100f;
+                aimTransform.position = targetPos;
+            }
         }
-        else
-        {
-            Vector3 targetPos = ray.origin + ray.direction * 100f;
-            aimTransform.position = targetPos;
-        }
-        Vector3 aimDirection = (aimTransform.position - turret.position).normalized;
-        Quaternion targetRotation = Quaternion.LookRotation(aimDirection);
-        targetRotation.x = 0f;
-        targetRotation.z = 0f;
-        turret.rotation = Quaternion.RotateTowards(turret.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+            Vector3 aimDirection = (aimTransform.position - turret.position).normalized;
+            Quaternion targetRotation = Quaternion.LookRotation(aimDirection);
+            targetRotation.x = 0f;
+            targetRotation.z = 0f;
+            turret.rotation = Quaternion.RotateTowards(turret.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+        
     }
     public void GunMove()
     {
@@ -56,5 +64,11 @@ public class TankTurretMovement : MonoBehaviour
         gun.rotation = Quaternion.RotateTowards(gun.rotation, targetRotation, Time.deltaTime * rotationSpeed);
     }
 
+    // 터렛 잠금 기능
+    void OnMouseRight(InputValue key)
+    {
+        print(key.isPressed);
+        isTurretLock = key.isPressed ? true : false;
+    }
 }
     

02f9a9077c8230f4feed7b0cc44390e8f8a41e86
changu0923 <152964087+changu0923@users.noreply.github.com>
2024-05-21 21:34:57 +0900

    Update UserData.cs

--- Assets/My Assets/Scripts/Database/UserData.cs
+++ Assets/My Assets/Scripts/Database/UserData.cs
@@ -38,12 +38,12 @@ public class UserData
         using (SHA256 sha256 = SHA256.Create())
         {
             byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
-            StringBuilder builder = new StringBuilder();
+            StringBuilder sb = new StringBuilder();
             for (int i = 0; i < bytes.Length; i++)
             {
-                builder.Append(bytes[i].ToString("x2"));
+                sb.Append(bytes[i].ToString("x2"));
             }
-            return builder.ToString();
+            return sb.ToString();
         }
     }
 }
